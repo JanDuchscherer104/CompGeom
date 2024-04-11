d0dac7125bef21f600e0b74d986b29b65e8bbe5a
fapfaff <fabian-pfaff@outlook.de>
2024-04-11 22:08:05 +0200

    feat: calc intersections and print to stdout

--- lab01/src/main.rs
+++ lab01/src/main.rs
@@ -1,14 +1,48 @@
 mod geometry;
-use std::{fs::File, io::{self, BufRead}, path::Path};
+mod naive_intersect;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+    time::Instant,
+};
 
 use geometry::{Line2D, Point2D};
 
+use crate::naive_intersect::get_intersections;
+
 fn main() {
-    println!("Hello, world!");
-    let file = ".data/s_1000_1.dat";
-    let lines = read_lines_from_file(file).unwrap();
+    let filenames = vec!["s_1000_1.dat", "s_10000_1.dat", "s_100000_1.dat"];
+
+    print_header();
+    for filename in filenames {
+        let lines = read_lines_from_file(&format!(".data/{}", filename)).unwrap();
+        let start_time = Instant::now();
+        let intersections = get_intersections(&lines);
+        let duration = start_time.elapsed().as_millis();
+        print_entry(
+            lines.len().try_into().unwrap(),
+            intersections.len().try_into().unwrap(),
+            duration.try_into().unwrap(),
+        )
+    }
 }
 
+fn print_header() {
+    println!("{}", "-".repeat(49));
+    println!(
+        "| {0: <10} | {1: <16} | {2: <13} |",
+        "# Lines", "# Intersections", "Duration (ms)"
+    );
+    println!("{}", "-".repeat(49));
+}
+
+fn print_entry(number_lines: u32, number_intersections: u32, duration_ms: u32) {
+    println!(
+        "| {0: <10} | {1: <16} | {2: <13} |",
+        number_lines, number_intersections, duration_ms
+    );
+}
 
 fn read_lines_from_file(file_name: &str) -> io::Result<Vec<Line2D>> {
     let path = Path::new(file_name);
