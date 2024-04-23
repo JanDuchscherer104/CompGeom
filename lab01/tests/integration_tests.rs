ee4b7c6c7bd195beefcb2da4951d603fcb4f605a
fapfaff <fabian-pfaff@outlook.de>
2024-04-23 20:42:41 +0200

    test: add integration test that compares calculation with external library

--- lab01/tests/integration_tests.rs
+++ lab01/tests/integration_tests.rs
@@ -1,4 +1,4 @@
-use lab01::{geometry::Line2D, utils::read_lines_from_file};
+use lab01::utils::read_lines_from_file;
 
 #[test]
 fn identical_number_of_intersections_for_internal_and_external_computation() {
@@ -8,7 +8,17 @@ fn identical_number_of_intersections_for_internal_and_external_computation() {
     let external_intersections = lab01::external_library::get_intersections_external(&lines);
 
     for intersection in internal_intersections.iter() {
-        println!("(({}, {}),({}, {})) x (({}, {}), ({},{}))", intersection.0.start.x, intersection.0.start.y, intersection.0.end.x, intersection.0.end.y, intersection.1.start.x, intersection.1.start.y, intersection.1.end.x, intersection.1.end.y);
+        println!(
+            "(({}, {}),({}, {})) x (({}, {}), ({},{}))",
+            intersection.0.start.x,
+            intersection.0.start.y,
+            intersection.0.end.x,
+            intersection.0.end.y,
+            intersection.1.start.x,
+            intersection.1.start.y,
+            intersection.1.end.x,
+            intersection.1.end.y
+        );
     }
 
     assert_eq!(internal_intersections.len(), external_intersections.len());
