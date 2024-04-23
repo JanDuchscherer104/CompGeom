3c6c5c8a2fffcbbb7fd2b856c9d2c48c680ff5d7
fapfaff <fabian-pfaff@outlook.de>
2024-04-23 17:25:30 +0200

    test: add integration test and introduce lib module

--- lab01/src/main.rs
+++ lab01/src/main.rs
@@ -1,14 +1,13 @@
-mod geometry;
-mod naive_intersect;
-mod external_library;
+pub mod utils;
+pub mod geometry;
+pub mod naive_intersect;
+pub mod external_library;
 use std::{
-    fs::File,
-    io::{self, BufRead},
-    path::Path,
     time::Instant,
 };
 
 use geometry::{Line2D, Point2D};
+use utils::read_lines_from_file;
 
 use crate::naive_intersect::get_intersections;
 use crate::external_library::get_intersections_external;
@@ -45,36 +44,4 @@ fn print_entry(number_lines: u32, number_intersections: u32, duration_ms: u32) {
         "| {0: <10} | {1: <16} | {2: <13} |",
         number_lines, number_intersections, duration_ms
     );
-}
-
-fn read_lines_from_file(file_name: &str) -> io::Result<Vec<Line2D>> {
-    let path = Path::new(file_name);
-    let file = File::open(&path).unwrap();
-    let reader = io::BufReader::new(file);
-
-    let mut lines: Vec<Line2D> = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        let nums: Vec<f64> = line
-            .split(' ')
-            .map(|num| num.trim().parse().unwrap())
-            .collect();
-        if nums.len() == 4 {
-            let point1 = Point2D {
-                x: nums[0],
-                y: nums[1],
-            };
-            let point2 = Point2D {
-                x: nums[2],
-                y: nums[3],
-            };
-            let line = Line2D {
-                start: point1,
-                end: point2,
-            };
-            lines.push(line);
-        }
-    }
-
-    Ok(lines)
 }
