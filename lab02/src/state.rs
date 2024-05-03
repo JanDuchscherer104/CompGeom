88119dfcb5bf45f40dcd9278106aec5acda5f8fd
fapfaff <fabian-pfaff@outlook.de>
2024-05-03 20:31:54 +0200

    feat(ueb02): add State#get_area()

--- lab02/src/state.rs
+++ lab02/src/state.rs
@@ -4,3 +4,9 @@ pub struct State {
     pub name: String,
     pub polygons: Vec<Polygon>,
 }
+
+impl State {
+    pub fn get_area(&self) -> f64 {
+        self.polygons.iter().map(|polygon| polygon.get_area()).sum()
+    }
+}
