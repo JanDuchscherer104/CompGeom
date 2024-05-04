10532a1d56d4d0c1a1b79fc932545741cfb24646
fapfaff <fabian-pfaff@outlook.de>
2024-05-04 10:05:17 +0200

    test(lab02): test State#get_area()

--- lab02/src/state.rs
+++ lab02/src/state.rs
@@ -10,3 +10,43 @@ impl State {
         self.polygons.iter().map(|polygon| polygon.get_area()).sum()
     }
 }
+
+mod tests {
+    use super::*;
+    use crate::geometry::point::Point2D;
+
+    #[test]
+    fn test_get_area_when_state_has_one_polygon() {
+        let mut state = State {
+            name: String::from("State"),
+            polygons: vec![Polygon::new()],
+        };
+
+        state.polygons[0].add_point(Point2D::new(0.0, 0.0));
+        state.polygons[0].add_point(Point2D::new(1.0, 0.0));
+        state.polygons[0].add_point(Point2D::new(1.0, 1.0));
+        state.polygons[0].add_point(Point2D::new(0.0, 1.0));
+
+        assert_eq!(state.get_area(), 1.0);
+    }
+
+    #[test]
+    fn test_get_area_when_state_has_multiple_polygons() {
+        let mut state = State {
+            name: String::from("State"),
+            polygons: vec![Polygon::new(), Polygon::new()],
+        };
+
+        state.polygons[0].add_point(Point2D::new(0.0, 0.0));
+        state.polygons[0].add_point(Point2D::new(1.0, 0.0));
+        state.polygons[0].add_point(Point2D::new(1.0, 1.0));
+        state.polygons[0].add_point(Point2D::new(0.0, 1.0));
+
+        state.polygons[1].add_point(Point2D::new(0.0, 0.0));
+        state.polygons[1].add_point(Point2D::new(1.0, 0.0));
+        state.polygons[1].add_point(Point2D::new(1.0, 1.0));
+        state.polygons[1].add_point(Point2D::new(0.0, 1.0));
+
+        assert_eq!(state.get_area(), 2.0);
+    }
+}
