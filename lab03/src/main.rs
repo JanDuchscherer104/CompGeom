f3e53705538b3257cdb693a304547fb78070e90b
Jan Duchscherer <duchsche@hm.edu>
2024-06-04 09:38:58 +0200

    feat: add util fn to parse *.dat paths

--- lab03/src/main.rs
+++ lab03/src/main.rs
@@ -1,5 +1,14 @@
-mod geometry;
+use utils::get_dat_paths;
+use geometry::line_segments::LineSegments2D;
 
-fn main() {
-    println!("Hello, world!");
+mod geometry;
+mod utils;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let paths = get_dat_paths("data")?;
+    for path in paths {
+        let lines = LineSegments2D::from_dat(&path)?;
+        println!("{:?}", lines);
+    }
+    Ok(())
 }
